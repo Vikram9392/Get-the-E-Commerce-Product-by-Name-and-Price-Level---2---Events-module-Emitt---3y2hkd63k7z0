533415a70844eb8860c5b77ac53dda221f3f9917
Vikram9392 <vikramvallala12@gmail.com>
2024-01-30 18:26:34 +0530

    done

--- src/app.js
+++ src/app.js
@@ -4,11 +4,19 @@ const app = express();
 
 // Importing products from products.json file
 const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
-
+console.log(products)
 //Middlewares
 app.use(express.json());
 
 // GET endpoint for sending the products to client by id
 //// Endpoint - /api/v1/products/:id
+app.get('/api/v1/products/:id',(req,res)=>{
+    res.status(200).json({
+        status: "success", 
+      message: "Product fetched successfully",
+       data: {products}
+    })
+
+    })
 
 module.exports = app;
